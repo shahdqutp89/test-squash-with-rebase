5bef67a3b3a8d7021573fd8dd72e5fe0395d7b02
maieffat806 <EXT.Mai.Effat@appraid-tech.com>
2025-09-10 16:42:39 +0300

    remove the last two lines
    remove one print line
    
    squashing the last 2 commits

--- testfile.py
+++ testfile.py
@@ -11,10 +11,5 @@ class Parrot:
 blu = Parrot("Blu", 10)
 woo = Parrot("Woo", 15)
 
-# access the class attributes
-print("Blu is a {}".format(blu.__class__.species))
 print("Woo is also a {}".format(woo.__class__.species))
 
-# access the instance attributes
-print("{} is {} years old".format( blu.name, blu.age))
-print("{} is {} years old".format( woo.name, woo.age))
